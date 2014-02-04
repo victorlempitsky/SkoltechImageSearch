d5ac52367ac91e0ba7e282ff024878f5eeb59bc7
Mikhail Barannikov <medan1989@gmail.com>
2014-02-04 09:42:42 +0400

    Mikhail Barannikov Local path
    Added path for mac.

--- getDataPath.m
+++ getDataPath.m
@@ -8,5 +8,7 @@ if strcmp(out(1:4), 'ts-l')
     datapath = '/mnt/Data/';
 elseif strcmp(out(1:8),'vilem-pc')
     datapath = 'E:\Code\ImageSearchData\';
+elseif strcmp(out(1:5), 'medan')
+    datapath = '/Users/mikhail/Projects/SkoltechImageSearch/';
 end  %insert your computer names here using elseif (mind the trailing symbol!)
     
