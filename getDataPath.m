f2955e860070796f84432917e6046c7b50570849
0minus273 <0minus273@gmail.com>
2014-02-06 03:38:48 -1100

    Update getDataPath.m

--- getDataPath.m
+++ getDataPath.m
@@ -8,6 +8,8 @@ if strcmp(out(1:4), 'ts-l')
     datapath = '/mnt/Data/';
 elseif strcmp(out(1:8),'vilem-pc')
     datapath = 'E:\Code\ImageSearchData\';
+elseif strcmp(out(1:8),'iceberg')
+    datapath = '/home/m273/SkVisual/Data';
 elseif strcmp(out(1:5), 'medan')
     datapath = '/Users/mikhail/Projects/SkoltechImageSearch/';
 end  %insert your computer names here using elseif (mind the trailing symbol!)
