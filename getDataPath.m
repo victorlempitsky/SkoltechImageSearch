7c1fc0cae095f5446d3cbe99ed8fb2860a90def1
0minus273 <0minus273@gmail.com>
2014-02-06 03:45:17 -1100

    Update getDataPath.m

--- getDataPath.m
+++ getDataPath.m
@@ -8,7 +8,7 @@ if strcmp(out(1:4), 'ts-l')
     datapath = '/mnt/Data/';
 elseif strcmp(out(1:8),'vilem-pc')
     datapath = 'E:\Code\ImageSearchData\';
-elseif strcmp(out(1:8),'iceberg')
+elseif strcmp(out(1:7),'iceberg')
     datapath = '/home/m273/SkVisual/Data';
 elseif strcmp(out(1:5), 'medan')
     datapath = '/Users/mikhail/Projects/SkoltechImageSearch/';
