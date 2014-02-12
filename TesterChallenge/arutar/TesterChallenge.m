bfd4d79df2ce4d8964905aa64815dcaea6292e00
arutar <medan1989@gmail.com>
2014-02-12 16:02:30 +0400

    TesterChallenge2

--- TesterChallenge/arutar/TesterChallenge.m
+++ TesterChallenge/arutar/TesterChallenge.m
@@ -3,21 +3,27 @@ close all;
 
 tic
 % folder with images
-Folder = '/Users/mikhail/Projects/images/';
+% Folder = '/Users/mikhail/Projects/images/';
+Folder = '/mnt/Images/6/';
 
-Imgs = dir([Folder '*.jpg']);
+Imgs = dir([Folder]);
 corner_size = 100;
 NumImgs = size(Imgs,1);
-idx = randi(NumImgs);
-disp(['Search index = ', num2str(idx)]);
+% idx = randi(NumImgs);
+% idx = 3;
+
+% disp(['Search index = ', num2str(idx)]);
 
 % read image
-img = double(imread([Folder Imgs(idx).name]));
-img = img(1:corner_size, 1:corner_size, :) ./ 255;
+img = double(imread(['/mnt/Data/taster/fragment1.png']));
+img = img ./ 255;
 
 % find image and display found index
 idx_ = FindImage(Folder, img);
 disp(['Found index = ', num2str(idx_)]);
+
+img_ = double(imread([Folder '/' Imgs(idx_).name]));
+imshow(img_./255);
 
 toc
 
