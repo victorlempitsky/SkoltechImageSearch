75086a51e6e73210a4c756c77d4c24083b92e5ca
mikhail-matrosov <mihail-matrosov@mail.ru>
2014-03-14 10:27:58 +0400

    resizeing

--- matrosov/LargeScale/backend.m
+++ matrosov/LargeScale/backend.m
@@ -1,5 +1,9 @@
 % usage: "query='imgs/1/123.456.jpg'; backend;"
 
+MAX_IMG_SIZE = 1024;
+
+tic;
+
 if ~exist('pqPcaEncodings')
     load ('/mnt/Data/VLADs/filenamesAll')
     load ('/mnt/Data/VLADs/clusters')
@@ -11,15 +15,30 @@ if ~exist('pqPcaEncodings')
     load ('/mnt/Data/VLADs/pqRotation')
 end
 
+toc;
+
 img = imread(query);
 
+% resize img if necessary
+sz = size(img);
+sz = sz([1,2]);
+if sz(1)>MAX_IMG_SIZE || sz(1)>MAX_IMG_SIZE
+    img = imresize(img, 1024.0/max(sz));
+end
+
+% get encoding
 [ pqpca, pca, vlad, sifts ] = getPqPcaVladFromImg( img, ...
 clusters, adaptedCenters, coeff, pqClusters, pqRotation );
 
+% measure distances
 distances = pq_alldist(pqpca, pqPcaEncodings, pqDistances);
 
+% rank
 [distances,ix] = sort(distances);
 
+toc;
+
+% write result
 fd = fopen('output.txt', 'w');
 fprintf(fd, '/mnt/Images/%s\n', filenames{ix(1:5)});
 fclose(fd);
--- matrosov/LargeScale/helloWorld.m
+++
@@ -1,8 +0,0 @@
-function [ output ] = helloWorld( str )
-%HELLOWORLD Summary of this function goes here
-%   Detailed explanation goes here
-
-output = ['Hello, ', str, '!'];
-
-end
-
