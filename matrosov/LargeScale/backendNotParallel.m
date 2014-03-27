fa3166fcf78f2e114281b480d2b1620be0ea6366
mikhail-matrosov <mihail-matrosov@mail.ru>
2014-03-27 20:35:10 +0400

    ptr to parallel, pca reranking

--- matrosov/LargeScale/backendNotParallel.m
+++ matrosov/LargeScale/backendNotParallel.m
@@ -1,8 +1,7 @@
-function ranks = backendNotParallel (query, ...
-    pqPcaEncodings, encodings, clusters, adaptedCenters, coeff, pqClusters, pqRotation, N_RESULTS)
+function ranks = backendNotParallel (query, d)
 
 MAX_IMG_SIZE = 1024;
-N = size(pqPcaEncodings,2);
+N = size(d.pqPcaEncodings,2);
 
 try
     img = imread(query);
@@ -22,35 +21,35 @@ end
 
 % get encoding
 [ pqpca, pca, vlad, sifts ] = getPqPcaVladFromImg( img, ...
-clusters, adaptedCenters, coeff, pqClusters, pqRotation );
+d.clusters, d.adaptedCenters, d.coeff, d.pqClusters, d.pqRotation );
 
 % measure distances - symmetric
-%distances = pq_alldist(pqpca, pqPcaEncodings, pqDistances);
+distances = pq_alldist(pqpca, d.pqPcaEncodings, d.pqDistances);
 
 % measure distances - assymmetric
-pqDistances = zeros(256,16);
-for j=1:16
-    ix = (j-1)*16+1 : j*16;
-    pqDistances(:,j) = vl_alldist2(pca(ix,:), pqClusters(:,:,j));
-end
-
-subDistances = zeros(N, 16);
-pq = int32(pqPcaEncodings)+1;
-for j=1:16
-    subDistances(:,j) = pqDistances(pq(j,:), j);
-end
-
-distances = sum(subDistances, 2);
+%pqDistances = zeros(256,16);
+%for j=1:16
+%     ix = (j-1)*16+1 : j*16;
+%     pqDistances(:,j) = vl_alldist2(pca(ix,:), pqClusters(:,:,j));
+% end
+% 
+% subDistances = zeros(N, 16);
+% pq = int32(pqPcaEncodings)+1;
+% for j=1:16
+%     subDistances(:,j) = pqDistances(pq(j,:), j);
+% end
+% 
+% distances = sum(subDistances, 2);
 
 [~,ix] = sort(distances);
 
-encs = encodings(:,ix(1:1000));
+encs = d.encodings(:,ix( 1:min(d.N_RESULTS*20, N) ));
 distances2 = vl_alldist2(pca, encs);
 
 [dd, ix2] = sort(distances2);
 
 ranks = zeros(size(distances));
-ranks(ix(ix2(1:N_RESULTS))) = 1./dd(1:N_RESULTS);
+ranks(ix(ix2(1:d.N_RESULTS))) = 1./dd(1:d.N_RESULTS);
 
 %distances = vl_alldist2(pca, pcaEncodings);
 
